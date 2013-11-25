4a8888e6c4d81445ed454919abe162c7505a704d
Zakiya <info@zakiyadesigns.com>
2013-11-24 21:14:09 -0800

    Removing the restriction on zooming for accessibility.

--- bling/template.php
+++ bling/template.php
@@ -13,7 +13,7 @@ function bling_preprocess_html(&$vars) {
     '#tag' => 'meta', 
     '#attributes' => array(
       'name' => 'viewport', 
-      'content' => 'width=device-width, initial-scale=1, maximum-scale=1',
+      'content' => 'width=device-width, initial-scale=1',
     ),
   );
   drupal_add_html_head($viewport, 'viewport');
